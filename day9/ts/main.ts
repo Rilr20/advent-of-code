262a22904b303239568c7aa556222d0f8155f69d
Rilr20 <nrrl99@gmail.com>
2024-08-21 23:25:25 +0200

    part 1 completed

--- day9/ts/main.ts
+++ day9/ts/main.ts
@@ -13,16 +13,16 @@ export default {
         let tailYPosition: number = 0
         let positionsVisited: string[] = []
         rows.forEach(row => {
-            console.log(row)
+            // console.log(row)
             let key: string = row.split(" ")[0]
             let amount: number = parseInt(row.split(" ")[1])
             for (let i = 0; i < amount; i++) {
-                console.log("key:" + key)
-                console.log("amount:" + amount)
-                console.log("i:" + i)
+                // console.log("key:" + key)
+                // console.log("amount:" + amount)
+                // console.log("i:" + i)
                 switch (key) {
                     case "R":
-                        headXPosition = headXPosition+ 1
+                        headXPosition = headXPosition + 1
                         break;
                     case "L":
                         headXPosition = headXPosition - 1
@@ -34,58 +34,83 @@ export default {
                         headYPosition = headYPosition - 1
                         break;
                 }
-            // for (let i = 0; i < amount-1; i++) {
+                // for (let i = 0; i < amount-1; i++) {
                 positionToString(headXPosition, headYPosition);
                 if (!tailRange(tailXPosition, tailYPosition, headXPosition, headYPosition)) {
-                    console.log("I am not adjacent")
+                    // console.log("I am not adjacent")
                     /**
                      * place tail position one behind the head, 
                      * if direction was up place -1 y
                      * if direction was down place +1 y
                      * if direction was right place -1 x
                      * if direction was left place +1 x
+                     * if it is diagonal then it should be +1 x +1 y et al.
                     */
                     switch (key) {
                         case "R":
-                            tailXPosition = tailXPosition + 1
+                            tailXPosition = headXPosition - 1
+                            tailYPosition = headYPosition;
                             break;
                         case "L":
-                            tailXPosition = tailXPosition - 1
+                            tailXPosition = headXPosition + 1
+                            tailYPosition = headYPosition;
                             break;
                         case "U":
-                            tailYPosition = tailYPosition + 1
+                            tailYPosition = headYPosition - 1
+                            tailXPosition = headXPosition;
                             break;
                         case "D":
-                            tailYPosition = tailYPosition - 1
+                            tailYPosition = headYPosition + 1
+                            tailXPosition = headXPosition;
                             break;
                     }
                 }
 
-                // positionsVisited.push(positionToString(tailXPosition, tailYPosition))
+                positionsVisited.push(positionToString(tailXPosition, tailYPosition))
+                // console.log(positionsVisited)
             }
 
         })
-        console.log(positionsVisited)
-        console.log(positionsVisited.length)
-        console.log("bitch");
+        // console.log("SLUTET ÄR NÄRA");
 
-        function tailRange(tailX: number, tailY: number, headX: number, headY: number, range: number = 1): boolean {
-            let xInterval = [headX + range, headX, headX - range]
-            let yInterval = [headY + range, headY, headY - range]
-            if (xInterval.includes(tailX) && yInterval.includes(tailY)) {
-                return true
-            }
-            return false
-        }
-        function positionToString(x: number, y: number): string {
-            console.log(`${x},${y}`)
-            return `${x},${y}`
-        }
-        return 0
+        positionsVisited = [...new Set(positionsVisited)]
+        // console.log(positionsVisited)
+        // console.log(positionsVisited.length)
+
+        return positionsVisited.length
     },
     part2(file: string): number {
         // Implementation
+        // some sort of linked list?
+        interface LinkedList {
+            positionX: number,
+            positionY: number,
+            visitedPositions: [],
+            child: LinkedList | null
+        }
+        let head: LinkedList = {
+            positionX: 0,
+            positionY: 0,
+            visitedPositions: [],
+            child: null
+        }
+        for (let i = 0; i < 9; i++) {
+                        
+        }
 
         return 0
-    }
+    },
+
 };
+
+function tailRange(tailX: number, tailY: number, headX: number, headY: number, range: number = 1): boolean {
+    let xInterval = [headX + range, headX, headX - range]
+    let yInterval = [headY + range, headY, headY - range]
+    if (xInterval.includes(tailX) && yInterval.includes(tailY)) {
+        return true
+    }
+    return false
+}
+function positionToString(x: number, y: number): string {
+    return `${x},${y}`
+}
