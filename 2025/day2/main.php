2cf2476ff15c1ea01ffa7de8bd2e39bbac008aaf
Rilr20 <nrrl99@gmail.com>
2025-12-02 18:16:40 +0100

    day 2 part 2 completed

--- 2025/day2/main.php
+++ 2025/day2/main.php
@@ -1,17 +1,19 @@
 <?php
 
 namespace AdventOfCode\day2;
-function part1($file):int {
+
+function part1($file): int
+{
     $groups = explode(",", $file);
     // var_dump($groups);
     $res = 0;
     foreach ($groups as $group) {
         # code...
-        [$start, $end] = explode("-",$group);
-        for ($i=$start; $i <= $end ; $i++) { 
+        [$start, $end] = explode("-", $group);
+        for ($i = $start; $i <= $end; $i++) {
             $len = strlen((string)$i);
-            if ($len% 2 == 0) {
-                $substart = substr($i,0,strlen($i)/2);
+            if ($len % 2 == 0) {
+                $substart = substr($i, 0, strlen($i) / 2);
                 $subend = substr($i, strlen($i) / 2, strlen($i));
                 if ($substart == $subend) {
                     $res += (int) $i;
@@ -23,6 +25,36 @@ function part1($file):int {
     return $res;
 }
 
-function part2($file):int {
-    return 0;
+function part2($file): int
+{
+    $groups = explode(",", $file);
+    $res = 0;
+
+    foreach ($groups as $group) {
+        [$start, $end] = explode("-", $group);
+        for ($i = $start; $i <= $end; $i++) {
+            $number = (string) $i;
+            $newstring = "";
+            $oppositestring = "";
+            for ($j = strlen($number) / 2; $j >= 1; $j--) {
+                $newstring = substr($number, 0, $j);
+                $oppositestring = substr($number, $j);
+                $exploded = explode($newstring, $oppositestring);
+                if (count($exploded) > 1) {
+                    $is_empty = true;
+                    foreach ($exploded as $item) {
+                        if ($item != "") {
+                            $is_empty = false;
+                            break;
+                        }
+                    }
+                    if ($is_empty) {
+                        $res += $i;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return $res;
 }
