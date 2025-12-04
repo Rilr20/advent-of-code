439788f548811e33c274013a0438516958e668d2
Rilr20 <nrrl99@gmail.com>
2025-12-05 00:22:21 +0100

    day4 part 2 completed

--- 2025/day4/main.php
+++ 2025/day4/main.php
@@ -63,5 +63,59 @@ function part1($file): int
 
 function part2($file): int
 {
-    return 0;
+
+    $rows = explode("\n", $file);
+    $accessable = 0;
+    $removed = true;
+
+    while ($removed) {
+        $removed = false;
+        for ($y = 0; $y < count($rows); $y++) {
+            $width = strlen($rows[$y]);
+                for ($x = 0; $x < $width; $x++) {
+                    $tp = 0;
+
+                    if ($y - 1 >= 0) {
+                        $tp += $rows[$y - 1][$x] == "@" ? 1 : 0;
+
+                        if ($x - 1 >= 0) {
+                            $tp += $rows[$y - 1][$x - 1] == "@" ? 1 : 0;
+                        }
+
+                        if ($x + 1 <= $width) {
+                            $tp += $rows[$y - 1][$x + 1] == "@" ? 1 : 0;
+                        }
+                    }
+
+                    if ($y + 1 <= count($rows) - 1) {
+                        $tp += $rows[$y + 1][$x] == "@" ? 1 : 0;
+
+                        if ($x - 1 >= 0) {
+                            $tp += $rows[$y + 1][$x - 1] == "@" ? 1 : 0;
+                        }
+
+                        if ($x + 1 <= $width) {
+                            $tp += $rows[$y + 1][$x + 1] == "@" ? 1 : 0;
+                        }
+                    }
+
+                    if ($x - 1 >= 0) {
+                        $tp += $rows[$y][$x - 1] == "@" ? 1 : 0;
+                    }
+                    if ($x + 1 <= $width) {
+                        $tp += $rows[$y][$x + 1] == "@" ? 1 : 0;
+                    }
+
+
+                    if ($rows[$y][$x] == "@" && $tp < 4) {
+                        // array_push($toiletpaper, [$x, $y]);
+                        $accessable++;
+                        $rows[$y][$x] = ".";
+                        $removed = true;
+                    }
+                }
+            }
+    }
+
+    return $accessable;
 }
